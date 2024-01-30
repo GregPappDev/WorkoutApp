6fc689fb4b33aa59e0d8b3dffd1f53fefb0f1812
GregPappDev <gregpappdev@gmail.com>
2024-01-30 21:55:56 +0100

    add static Validation class

---
+++ WorkoutAppApi/WorkoutAppApi/Utils/Validation.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutAppApi.Models.DTOs.Excercise;
+
+namespace WorkoutAppApi.Utils
+{
+    public static class Validation<T>
+    {
+        public static bool ValidateNotNull(T input) 
+        { 
+            return input != null;
+        }
+
+
+    }
+}
