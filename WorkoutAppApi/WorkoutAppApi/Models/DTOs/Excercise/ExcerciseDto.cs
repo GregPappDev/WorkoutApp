572bac4eaa1fec2d257f2c692da70a2927cfaa65
GregPappDev <gregpappdev@gmail.com>
2024-01-30 11:49:29 +0100

    add IExcercise interface

---
+++ WorkoutAppApi/WorkoutAppApi/Models/DTOs/Excercise/ExcerciseDto.cs
@@ -0,0 +1,11 @@
+﻿using WorkoutAppApi.Models.Enums;
+
+namespace WorkoutAppApi.Models.DTOs.Excercise
+{
+    public class ExcerciseDto
+    {
+        public required User User { get; set; }
+        public required string Name { get; set; }
+        public required ExcerciseType Type { get; set; }
+    }
+}
---
+++ WorkoutAppApi/WorkoutAppApi/Services/Interfaces/IExcercise.cs
@@ -0,0 +1,14 @@
+﻿using WorkoutAppApi.Models;
+using WorkoutAppApi.Models.DTOs.Excercise;
+
+namespace WorkoutAppApi.Services.Interfaces
+{
+    public interface IExcercise
+    {
+        Task<IQueryable<Excercise>> GetAll();
+        Task<IQueryable<Excercise>> GetUserExcercises(string UserId);
+        Task<Excercise> Create(ExcerciseDto newExcercise);
+        Task<Excercise> Update(Guid Id, ExcerciseDto newExcercise);
+        Task<Excercise> Delete(Guid Id);
+    }
+}
